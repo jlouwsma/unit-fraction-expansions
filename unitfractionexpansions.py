3d65904fd21809a045441cc17e817a5e62efb3b1
Joel Louwsma <125417780+jlouwsma@users.noreply.github.com>
2024-07-07 23:35:36 -0600

    Update unitfractionexpansions.py
    Update to version 0.2. Changes extend functionality by allowing k to be any positive integer that is not a power of 2 rather than an odd integer at least 3.

--- unitfractionexpansions.py
+++ unitfractionexpansions.py
@@ -14,91 +14,223 @@ def Move1L(solution):
 
 def Move2NewTopRow(solution,k):
     newsolution=deepcopy(solution)
-    newrow=[k,0,0]
-    newsolution[-1][0]=newsolution[-1][0]-1
+    if (k%4==0):
+        newrow=[int(k/4),0,0]
+        newsolution[-1][2]=newsolution[-1][2]-1
+    if (k%4==1 or k%4==3):
+        newrow=[k,0,0]
+        newsolution[-1][0]=newsolution[-1][0]-1
+    if (k%4==2):
+        newrow=[int(k/2),0,0]
+        newsolution[-1][1]=newsolution[-1][1]-1
     return(newsolution+[newrow])
 
 def Move2SameTopRow(solution,k):
     newsolution=deepcopy(solution)
-    newsolution[-2][0]=newsolution[-2][0]-1
-    newsolution[-1][0]=newsolution[-1][0]+k
+    if (k%4==0):
+        newsolution[-2][2]=newsolution[-2][2]-1
+        newsolution[-1][0]=newsolution[-1][0]+int(k/4)
+    if (k%4==1 or k%4==3):
+        newsolution[-2][0]=newsolution[-2][0]-1
+        newsolution[-1][0]=newsolution[-1][0]+k
+    if (k%4==2):
+        newsolution[-2][1]=newsolution[-2][1]-1
+        newsolution[-1][0]=newsolution[-1][0]+int(k/2)
     return newsolution
 
 def Move3NewTopRow(solution,k):
     newsolution=deepcopy(solution)
-    newsolution[-1][2]=newsolution[-1][2]-1
+    if (k%4==0):
+        raise Exception("Move 3 not defined when k%4==0")
     if (k%4==1):
+        newsolution[-1][2]=newsolution[-1][2]-1
         newrow=[int((k-1)/4),0,1]
+    if (k%4==2):
+        raise Exception("Move 3 not defined when k%4==2")
     if (k%4==3):
-        newrow=[int((k-3)/4),1,1]
+        newsolution[-1][1]=newsolution[-1][1]-1
+        newsolution[-1][2]=newsolution[-1][2]-1
+        newrow=[int((3*k-1)/4),0,1]
     return(newsolution+[newrow])
 
 def Move3SameTopRow(solution,k):
     newsolution=deepcopy(solution)
-    newsolution[-2][2]=newsolution[-2][2]-1
+    if (k%4==0):
+        raise Exception("Move 3 not defined when k%4==0")
     if (k%4==1):
+        newsolution[-2][2]=newsolution[-2][2]-1
         newsolution[-1][0]=newsolution[-1][0]+int((k-1)/4)
+        newsolution[-1][2]=newsolution[-1][2]+1
+    if (k%4==2):
+        raise Exception("Move 3 not defined when k%4==2")
     if (k%4==3):
+        newsolution[-2][1]=newsolution[-2][1]-1
+        newsolution[-2][2]=newsolution[-2][2]-1
         newsolution[-1][0]=newsolution[-1][0]+int((k-3)/4)
-        newsolution[-1][1]=newsolution[-1][1]+1
-    newsolution[-1][2]=newsolution[-1][2]+1
+        newsolution[-1][2]=newsolution[-1][2]+1
     return newsolution
 
 def Move4NewTopRow(solution,k):
     newsolution=deepcopy(solution)
-    newsolution[-1][1]=newsolution[-1][1]-1
-    newrow=[int((k-1)/2),1,0]
+    if (k%4==0):
+        raise Exception("Move 4 not defined when k%4==0")
+    if (k%4==1 or k%4==3):
+        newsolution[-1][1]=newsolution[-1][1]-1
+        newrow=[int((k-1)/2),1,0]
+    if (k%4==2):
+        newsolution[-1][2]=newsolution[-1][2]-1
+        newrow=[int((k-2)/4),1,0]
     return(newsolution+[newrow])
 
 def Move4SameTopRow(solution,k):
     newsolution=deepcopy(solution)
-    newsolution[-2][1]=newsolution[-2][1]-1
-    newsolution[-1][0]=newsolution[-1][0]+int((k-1)/2)
-    newsolution[-1][1]=newsolution[-1][1]+1
+    if (k%4==0):
+        raise Exception("Move 4 not defined when k%4==0")
+    if (k%4==1 or k%4==3):
+        newsolution[-2][1]=newsolution[-2][1]-1
+        newsolution[-1][0]=newsolution[-1][0]+int((k-1)/2)
+        newsolution[-1][2]=newsolution[-1][2]+1
+    if (k%4==2):
+        newsolution[-2][2]=newsolution[-2][2]-1
+        newsolution[-1][0]=newsolution[-1][0]+int((k-2)/4)
+        newsolution[-1][2]=newsolution[-1][2]+1
     return newsolution
 
 def Move5NewTopRow(solution,k):
     newsolution=deepcopy(solution)
-    newsolution[-1][1]=newsolution[-1][1]-1
-    newsolution[-1][2]=newsolution[-1][2]-1
+    if (k%4==0):
+        raise Exception("Move 5 not defined when k%4==0")
     if (k%4==1):
+        newsolution[-1][1]=newsolution[-1][1]-1
+        newsolution[-1][2]=newsolution[-1][2]-1
         newrow=[int((3*k-3)/4),1,1]
+    if (k%4==2):
+        raise Exception("Move 5 not defined when k%4==2")
     if (k%4==3):
-        newrow=[int((3*k-1)/4),0,1]
+        newsolution[-1][2]=newsolution[-1][2]-1
+        newrow=[int((k-3)/4),1,1]
     return(newsolution+[newrow])
 
 def Move5SameTopRow(solution,k):
     newsolution=deepcopy(solution)
-    newsolution[-2][1]=newsolution[-2][1]-1
-    newsolution[-2][2]=newsolution[-2][2]-1
+    if (k%4==0):
+        raise Exception("Move 5 not defined when k%4==0")
     if (k%4==1):
+        newsolution[-2][1]=newsolution[-2][1]-1
+        newsolution[-2][2]=newsolution[-2][2]-1
         newsolution[-1][0]=newsolution[-1][0]+int((3*k-3)/4)
         newsolution[-1][1]=newsolution[-1][1]+1
+        newsolution[-1][2]=newsolution[-1][2]+1
+    if (k%4==2):
+        raise Exception("Move 5 not defined when k%4==2")
     if (k%4==3):
-        newsolution[-1][0]=newsolution[-1][0]+int((3*k-1)/4)
-    newsolution[-1][2]=newsolution[-1][2]+1
+        newsolution[-2][2]=newsolution[-2][2]-1
+        newsolution[-1][0]=newsolution[-1][0]+int((k-3)/4)
+        newsolution[-1][1]=newsolution[-1][1]+1
+        newsolution[-1][2]=newsolution[-1][2]+1
     return newsolution
+
+def Move2AdditionalTerms(k):
+    if (k%4==0):
+        return(int(k/4-1))
+    if (k%4==1 or k%4==3):
+        return(int(k-1))
+    if (k%4==2):
+        return(int(k/2-1))
+    
+def Move3AdditionalTerms(k):
+    if (k%4==0):
+        raise Exception("Move 3 not defined when k%4==0")
+    if (k%4==1):
+        return(int((k-1)/4))
+    if (k%4==2):
+        raise Exception("Move 3 not defined when k%4==2")
+    if (k%4==3):
+        return(int((3*k-5)/4))
+    
+def Move4AdditionalTerms(k):
+    if (k%4==0):
+        raise Exception("Move 4 not defined when k%4==0")
+    if (k%4==1 or k%4==3):
+        return(int((k-1)/2))
+    if (k%4==2):
+        return(int((k-2)/4))
+    
+def Move5AdditionalTerms(k):
+    if (k%4==0):
+        raise Exception("Move 5 not defined when k%4==0")
+    if (k%4==1):
+        return(int((3*k-3)/4))
+    if (k%4==2):
+        raise Exception("Move 5 not defined when k%4==2")
+    if (k%4==3):
+        return(int((k+1)/4))
+
+def Move1RIsApplicable(solution):
+    return(solution[-1][1]>0)
+
+def Move1LIsApplicable(solution):
+    return(solution[-1][0]>0)
+
+def Move2NewTopRowIsApplicable(solution,k):
+    return ((k%4==0 and solution[-1][2]>0) or ((k%4==1 or k%4==3) and solution[-1][0]>0) or (k%4==2 and solution[-1][1]>0))
+
+def Move2SameTopRowIsApplicable(solution,k):
+    return ((k%4==0 and solution[-2][2]>0) or ((k%4==1 or k%4==3) and solution[-2][0]>0) or (k%4==2 and solution[-2][1]>0))
+
+def Move3NewTopRowIsApplicable(solution,k):
+    return ((k%4==1 and solution[-1][2]>0) or (k%4==3 and solution[-1][1]>0 and solution[-1][2]>0))
+
+def Move3SameTopRowIsApplicable(solution,k):
+    return ((k%4==1 and solution[-2][2]>0) or (k%4==3 and solution[-2][1]>0 and solution[-2][2]>0))
+
+def Move4NewTopRowIsApplicable(solution,k):
+    return (((k%4==1 or k%4==3) and solution[-1][1]>0) or ((k%4==2) and solution[-1][2]>0))
+
+def Move4SameTopRowIsApplicable(solution,k):
+    return (((k%4==1 or k%4==3) and solution[-2][1]>0) or ((k%4==2) and solution[-2][2]>0))
+
+def Move5NewTopRowIsApplicable(solution,k):
+    return ((k%4==1 and solution[-1][1]>0 and solution[-1][2]>0) or (k%4==3 and solution[-1][2]>0))
+
+def Move5SameTopRowIsApplicable(solution,k):
+    return ((k%4==1 and solution[-2][1]>0 and solution[-2][2]>0) or (k%4==3 and solution[-2][2]>0))
 
 def Move1LIsPriorityExpansion(solution):
     return(solution[-1][2]<2)
 
-def Move2IsPriorityExpansion(solution):
+def Move2SameTopRowIsPriorityExpansion(solution,k):
     return(solution[-1][1]<2 and solution[-1][2]<2)
 
-def Move3IsPriorityExpansion(solution,k):
+def Move3SameTopRowIsPriorityExpansion(solution,k):
+    if (k%4==0):
+        return(False)
     if (k%4==1):
         return(solution[-1][2]==0 and solution[-1][1]<2 and solution[-1][0]+(k-1)/4<k)
-    if (k%4==3):
-        return(solution[-1][2]==0 and solution[-1][1]==0 and solution[-1][0]+(k-3)/4<k)
-
-def Move4IsPriorityExpansion(solution,k):
-    return(solution[-1][2]==0 and solution[-1][1]==0 and solution[-1][0]+(k-1)/2<k)
-
-def Move5IsPriorityExpansion(solution,k):
-    if (k%4==1):
+    if (k%4==2):
         return(False)
     if (k%4==3):
-        return(solution[-1][2]==0 and solution[-1][1]==0 and solution[-1][0]+(3*k-1)/4<k)
+        return(solution[-1][2]==0 and solution[-1][1]<2 and solution[-1][0]+(3*k-1)/4<k)
+
+def Move4SameTopRowIsPriorityExpansion(solution,k):
+    if (k%4==0):
+        return(False)
+    if (k%4==1):
+        return(solution[-1][2]==0 and solution[-1][1]==0 and solution[-1][0]+(k-1)/2<k)
+    if (k%4==2):
+        return(solution[-1][2]<2 and solution[-1][1]==0 and solution[-1][0]+(k-2)/4<k/2)
+    if (k%4==3):
+        return(solution[-1][2]<2 and solution[-1][1]==0 and solution[-1][0]+(k-1)/2<k and (solution[-1][2]==0 or solution[-1][0]+(k-1)/2<(3*k-1)/4))
+
+def Move5SameTopRowIsPriorityExpansion(solution,k):
+    if (k%4==0):
+        return(False)
+    if (k%4==1):
+        return(False)
+    if (k%4==2):
+        return(False)
+    if (k%4==3):
+        return(solution[-1][2]==0 and solution[-1][1]==0 and solution[-1][0]+(k-3)/4<k and solution[-1][0]+(k-3)/4<(3*k-1)/4 and solution[-1][0]+(k-3)/4<(k-1)/2)
 
 def FindSolutions(k,n):
     solutionlist=[[] for i in range(n+1)]
@@ -106,41 +238,37 @@ def FindSolutions(k,n):
     solutionlist[1]=[[[1,0,0]]]
     for i in range(2,n+1):
         for solution in solutionlist[i-1]:
-            if (solution[-1][1]>0):
+            if (Move1RIsApplicable(solution)):
                 solutionlist[i].append(Move1R(solution))
-            if (solution[-1][0]>0 and Move1LIsPriorityExpansion(solution)):
+            if (Move1LIsApplicable(solution) and Move1LIsPriorityExpansion(solution)):
                 solutionlist[i].append(Move1L(solution))
-        if (i>k-1):
-            for solution in solutionlist[i-(k-1)]:
-                if (solution[-1][0]>0):
+        if (i>Move2AdditionalTerms(k)):
+            for solution in solutionlist[i-Move2AdditionalTerms(k)]:
+                if (Move2NewTopRowIsApplicable(solution,k)):
                     solutionlist[i].append(Move2NewTopRow(solution,k))
                 if (len(solution)>=2):
-                    if (solution[-2][0]>0 and Move2IsPriorityExpansion(solution)):
+                    if (Move2SameTopRowIsApplicable(solution,k) and Move2SameTopRowIsPriorityExpansion(solution,k)):
                         solutionlist[i].append(Move2SameTopRow(solution,k))
-        if (k%4==1):
-            additionalterms=int((k-1)/4)
-        if (k%4==3):
-            additionalterms=int((k+1)/4)
-        if (i>additionalterms):
-            for solution in solutionlist[i-additionalterms]:
-                if (solution[-1][2]>0):
+        if ((k%4==1 or k%4==3) and i>Move3AdditionalTerms(k)):
+            for solution in solutionlist[i-Move3AdditionalTerms(k)]:
+                if (Move3NewTopRowIsApplicable(solution,k)):
                     solutionlist[i].append(Move3NewTopRow(solution,k))
                 if (len(solution)>=2):
-                    if (solution[-2][2]>0 and Move3IsPriorityExpansion(solution,k)):
+                    if (Move3SameTopRowIsApplicable(solution,k) and Move3SameTopRowIsPriorityExpansion(solution,k)):
                         solutionlist[i].append(Move3SameTopRow(solution,k))
-        if (i>(k-1)/2):
-            for solution in solutionlist[i-int((k-1)/2)]:
-                if (solution[-1][1]>0):
+        if (k%4!=0 and i>Move4AdditionalTerms(k)):
+            for solution in solutionlist[i-Move4AdditionalTerms(k)]:
+                if (Move4NewTopRowIsApplicable(solution,k)):
                     solutionlist[i].append(Move4NewTopRow(solution,k))
                 if (len(solution)>=2):
-                    if (solution[-2][1]>0 and Move4IsPriorityExpansion(solution,k)):
+                    if (Move4SameTopRowIsApplicable(solution,k) and Move4SameTopRowIsPriorityExpansion(solution,k)):
                         solutionlist[i].append(Move4SameTopRow(solution,k))
-        if (k%4==3 and i>(3*k-5)/4):
-            for solution in solutionlist[i-int((3*k-5)/4)]:
-                if(solution[-1][1]>0 and solution[-1][2]>0):
+        if (k%4==3 and i>Move5AdditionalTerms(k)):
+            for solution in solutionlist[i-Move5AdditionalTerms(k)]:
+                if (Move5NewTopRowIsApplicable(solution,k)):
                     solutionlist[i].append(Move5NewTopRow(solution,k))
                 if (len(solution)>=2):
-                    if (solution[-2][1]>0 and solution[-2][2]>0 and Move5IsPriorityExpansion(solution,k)):
+                    if (Move5SameTopRowIsApplicable(solution,k) and Move5SameTopRowIsPriorityExpansion(solution,k)):
                         solutionlist[i].append(Move5SameTopRow(solution,k))
     return(solutionlist[n])
 
